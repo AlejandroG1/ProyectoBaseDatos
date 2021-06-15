26ecac95478583dabee0db7ba634e8913ea44f28
AlejandroG1 <jalegalarza@gmail.com>
2021-06-15 14:57:10 -0500

    finalizado

--- views/usuarios/producto_agregar.php
+++ views/usuarios/producto_agregar.php
@@ -12,14 +12,14 @@
 <div class="col-sm-6">
 <div class="mb-3">
 <label for="nombre" class="form-label">Nombre *</label>
-<input type="text"  id="nombre" name="nombre" class="form-control">
+<input type="text"  id="nombre" name="nombre" class="form-control" required>
 </div>
 </div>
 
 <div class="col-sm-6">
 <div class="mb-3">
 <label for="descripcion" class="form-label">Descripcion *</label>
-<input type="text"  id="descripcion" name="descripcion" class="form-control">
+<input type="text"  id="descripcion" name="descripcion" class="form-control" required >
 </div>
 </div>
 </div>
@@ -28,14 +28,14 @@
 <div class="col-sm-6">
 <div class="mb-3">
 <label for="color" class="form-label">Color *</label>
-<input type="text"  id="color" name="color" class="form-control">
+<input type="text"  id="color" name="color" class="form-control" required>
 </div>
 </div>
 
 <div class="col-sm-6">
 <div class="mb-3">
 <label for="precio" class="form-label">Precio *</label>
-<input type="number"  id="precio" name="precio" class="form-control">
+<input type="number"  id="precio" name="precio" class="form-control" required>
 </div>
 </div>
 </div>
@@ -44,7 +44,7 @@
 <div class="col-sm-6">
 <div class="mb-3">
 <label for="cantidad" class="form-label">Cantidad *</label>
-<input type="number"  id="cantidad" name="cantidad" class="form-control">
+<input type="number"  id="cantidad" name="cantidad" class="form-control" required>
 </div>
 </div>
 
@@ -52,7 +52,7 @@
 
 <div class="mb-3">
 <label for="cantidad" class="form-label">Cantidad minima *</label>
-<input type="number"  id="cantidad_min" name="cantidad_min" class="form-control">
+<input type="number"  id="cantidad_min" name="cantidad_min" class="form-control" required> 
 </div>
 
 
@@ -62,7 +62,7 @@
     <div class="col-sm-12">
     <div class="mb-3">
 <label for="categorias" class="form-label">Categorias *</label>
-<select name="categorias" id="categorias" class="form-control">
+<select name="categorias" id="categorias" class="form-control" required>
     <option value="electronico">electronico</option>
     <option value="cocina">cocina</option>
     <option value="farmaceutico">farmaceutico</option>
@@ -81,7 +81,7 @@
 <div class="row">
         <div class="col-sm-12">
             <div class="form-group">
-                <input type="file" class="form-control-file" name="foto" id="foto">
+                <input type="file" class="form-control-file" name="foto" id="foto" required>
             </div>
         </div>
     </div>
